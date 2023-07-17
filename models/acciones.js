45b0a5a84bfc639437f12a678920b7fbcce52732
EstebanDarioRuizParedesCAMPUS <edrp.campus15@gmail.com>
2023-07-16 20:40:24 -0500

    v1

--- models/acciones.js
+++ models/acciones.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
 const accionesShema = mongoose.Schema({
-    tipoAccion: {
-        type: String,
-        required: true,
-        trim:true
-    },
-    valorAccion: {
-        type: String,
-        required: true,
-        trim:true
-    },
     porcentajeAccion: {
         type: String,
         required: true,
-        trim:true
+        trim:true,
     },
+    valorAccion: {
+        type: String,
+        required:true,
+        trim:true,
+    },
+    tipoAccion :{
+        type: String,
+        required:true,
+        trim:true,  
+    }
 },
 {
     timestamps: true,
